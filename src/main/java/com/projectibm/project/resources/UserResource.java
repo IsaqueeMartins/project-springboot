21a396e018f03caba6cd71e5df40832946385214
Isaque Martins <isaque.martins.barros@gmail.com>
2023-06-26 22:24:03 -0300

    eh isso 1.0

---
+++ src/main/java/com/projectibm/project/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.projectibm.project.resources;
+
+import com.projectibm.project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Ana Julia", "anajulia@gmail.com", "9999999", "123456");
+        return ResponseEntity.ok().body(u);
+    }
+}
