bebf8057d1a4563a4d2377b97b74d6db0d4f6c53
Isaque Martins <isaque.martins.barros@gmail.com>
2023-06-28 19:50:34 -0300

    eh isso 0.1

--- src/main/java/com/projectibm/project/entities/User.java
+++ src/main/java/com/projectibm/project/entities/User.java
@@ -1,11 +1,20 @@
 package com.projectibm.project.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -16,12 +25,12 @@ public class User implements Serializable {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
-            super();
-            this.id = id;
-            this.name = name;
-            this.email = email;
-            this.phone = phone;
-            this.password = password;
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
     }
     public Long getId() {
         return id;
