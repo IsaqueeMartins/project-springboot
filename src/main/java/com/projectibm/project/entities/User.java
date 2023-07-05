b3a39591782d37fd71acb504d432e945177150f7
Isaque Martins <isaque.martins.barros@gmail.com>
2023-07-04 21:14:06 -0300

    eh isso 0.1.2 (in progress)

--- src/main/java/com/projectibm/project/entities/User.java
+++ src/main/java/com/projectibm/project/entities/User.java
@@ -1,10 +1,10 @@
 package com.projectibm.project.entities;
 
 import jakarta.persistence.*;
-
 import java.io.Serial;
 import java.io.Serializable;
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -20,6 +20,8 @@ public class User implements Serializable {
     private String email;
     private String password;
     private String phone;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -62,6 +64,8 @@ public class User implements Serializable {
     public void setPassword(String Password) {
         this.password = password;
     }
+    @OneToMany(mappedBy = "client")
+    public List<Order> getOrders() { return orders; }
 
     public int hashCode() {
         final int prime = 31;
@@ -69,6 +73,7 @@ public class User implements Serializable {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
+    @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
